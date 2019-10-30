f3767e3f0ffe7b74d377e221b6832f09a8b1f8cf
Gregori Spielmann <gregorispielmann@MacBook-Pro-de-Gregori.local>
2019-10-30 10:31:31 -0300

    includes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './config/ReactotronConfig';
+
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
